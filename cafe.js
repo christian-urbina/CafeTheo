2b48725fc02a690a5e5adf9482b114352490cfe6
Christian Urbina <christianurbinaios@gmail.com>
2026-01-31 21:52:11 -0600

    Update cafe.js

--- cafe.js
+++ cafe.js
@@ -2,7 +2,7 @@ const cafeCarousel = document.querySelector('#cafeCarousel');
 
 if (cafeCarousel) {
     const carousel = new bootstrap.Carousel(cafeCarousel, {
-        interval: 4000,
+        interval: 2000,
         ride: 'carousel',
         pause: 'hover',
         touch: true
